2cd5c13ff8c3103473e5c8e48031a69fe38b40ab
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-02 20:30:16 -0500

    add next_node value and test

--- lib/node.rb
+++ lib/node.rb
@@ -1,6 +1,7 @@
 class Node
-  attr_reader :data
-  def initialize(data)
+  attr_reader :data, :next_node
+  def initialize(data, next_node)
     @data = data
+    @next_node = next_node
   end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -4,9 +4,10 @@ require './lib/node'
 
 RSpec.describe Node do
 it 'exists' do
-  node = Node.new("boop")
+  node = Node.new("boop", nil)
 require 'pry'; binding.pry
   expect(node).to be_instance_of(Node)
   expect(node.data).to eq ("boop")
+  expect(node.next_node).to eq (nil)
   end
 end
