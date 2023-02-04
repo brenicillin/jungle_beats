802fcfee85fb6f6902849d4a0765905a1a770f47
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-04 11:33:44 -0500

    add find test

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -50,4 +50,17 @@ RSpec.describe LinkedList do
   expect(list.count).to eq (4)
   expect(list.to_string).to eq("dop woo plop suu")
   end
+
+  it 'can find and return specific node data' do
+    list = LinkedList.new
+    list.append("deep")
+    list.append("woo")
+    list.append("blop")
+    list.insert(2, "shi")
+    list.insert(3, "shu")
+    require 'pry'; binding.pry
+    expect(list.to_string).to eq("deep woo shi shu blop")
+    expect(list.find(2, 1)).to eq("shi")
+    expect(list.find(1, 3)).to eq("woo shi shu")
+  end
 end
