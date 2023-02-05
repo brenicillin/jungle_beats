8699853ada730eaf2c88f9481a68942a0dd17680
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-05 13:35:18 -0500

    add jungle beat class

---
+++ lib/jungle_beats.rb
@@ -0,0 +1,19 @@
+class JungleBeat
+  attr_reader :list
+  def initialize
+    @list = LinkedList.new
+  end
+
+  def append(data)
+    array = data.split(' ')
+    array.each do |sound|
+      list.append(sound)
+    end
+    data
+  end
+
+  def count
+    new_count = @list.count
+    new_count
+  end
+end
